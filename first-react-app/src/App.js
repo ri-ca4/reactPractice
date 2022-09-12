99cc2f96f73b32312c205ea6f326065e1d293662
ri-ca4 <riley.de12@gmail.com>
2022-09-12 16:05:43 -0400

    create first-react-app

---
+++ first-react-app/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import ClassBasedComponent from './components/ClassBasedComponent';
+
+function App() {
+  return (
+      <ClassBasedComponent/>
+  );
+}
+
+export default App;
---
+++ first-react-app/src/components/ClassBasedComponent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Component } from "react";
+
+class ClassBasedComponent extends Component {
+    render(){
+        return(
+            <div>This is a class based component</div>
+        )
+    }
+};
+
+export default ClassBasedComponent;
