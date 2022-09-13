31ab53abc5ff8d0eb530ab66057daa0263845fb9
ri-ca4 <riley.de12@gmail.com>
2022-09-13 19:32:01 -0400

    practice with hooks useeffect

--- first-react-app/src/components/HooksPracticeComponent.jsx
+++ first-react-app/src/components/HooksPracticeComponent.jsx
@@ -6,22 +6,41 @@
 
 
 import React from "react";
+import { useEffect } from "react";
 import { useState } from "react";
 
 const HooksPracticeComponent = ()=>{
 
-    const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
+    const [flag, setFlag] = useState(false);
 
     const handleClick = ()=> {
         setCount(count + 1);
     }
 
-    console.log(count)
+    useEffect((()=>{
+        console.log("effect is called")
+    }),[])//=> componentdidmount/ called on load
+
+    useEffect((()=>{
+        if(count === 5){
+            setFlag(true)
+        }
+    }),[count])//=> componentdidupdate
+
+    useEffect((()=>{
+        return()=>{
+            console.log('unmounted')
+        }
+    }), )//=>componentwillunmount
 
     return(
         <div>
                 <button onClick={handleClick}>Click</button>
                 counter value is {count}
+                {
+                    flag && <p>flag is true</p>
+                }
         </div>
     )
 }
