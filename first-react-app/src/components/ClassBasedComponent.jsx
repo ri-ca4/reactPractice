de541078d7e1634c6d301f7b096f668d9d6827df
ri-ca4 <riley.de12@gmail.com>
2022-09-13 17:19:37 -0400

    practice with forms and events

--- first-react-app/src/components/ClassBasedComponent.jsx
+++ first-react-app/src/components/ClassBasedComponent.jsx
@@ -4,9 +4,11 @@ import { Component } from "react";
 class ClassBasedComponent extends Component {
     
     state = {
-        //write state properties here
-        count : 0, //initial value
-        flag  : false
+        formData : {
+            name : '',
+            age : 0
+        },
+        finalFormData : {}
     }
 
     // constructor(props){
@@ -18,46 +20,88 @@ class ClassBasedComponent extends Component {
     //     this.handleClick = this.handleClick.bind(this)
     // }
 
-    handleClick = () => {
-        //this.state.count = this.state.count + 1 <--- WRONG, directly mutating state
-        //USE setState methond!!!!!
-        const {count} = this.state;
-        this.setState({
-            count : count + 1
-        }, () => {
-            console.log(count)
-        })
+    // handleClick = () => {
+    //     //this.state.count = this.state.count + 1 <--- WRONG, directly mutating state
+    //     //USE setState methond!!!!!
+    //     const {count} = this.state;
+    //     this.setState({
+    //         count : count + 1
+    //     }, () => {
+    //         console.log(count)
+    //     })
+    // }
+
+    // componentDidMount(){
+    //     console.log("component mounted")
+    // }
+
+    // componentDidUpdate(prevProps, prevState){
+    //     console.log(prevState, this.state)
+    //     if (prevState && prevState.count !== this.state.count && this.state.count === 10){
+    //         this.setState({
+    //             flag : true
+    //         })
+    //     }
+    // }
+
+    // componentWillUnmount(){
+    //     //cleanup before component is destroyed
+    // }
+
+    handleAgeChange = (event)=> {
+        const {value} = event.target;
+        this.setState(prevState=>({
+                formData : {
+                    ...prevState.formData,
+                    age : value
+                }
+            }))
     }
 
-    componentDidMount(){
-        console.log("component mounted")
+    handleNameChange = (event)=> {
+        const {value} = event.target;
+        this.setState(prevState=>({
+                formData : {
+                    ...prevState.formData,
+                    name : value
+                }
+            }))
     }
 
-    componentDidUpdate(prevProps, prevState){
-        console.log(prevState, this.state)
-        if (prevState && prevState.count !== this.state.count && this.state.count === 10){
-            this.setState({
-                flag : true
-            })
-        }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        const {formData} = this.state
+        this.setState(prevState=>({
+            ...prevState,
+            finalFormData : {
+                ...prevState.finalFormData,
+                ...formData
+            },
+            formData : {
+                name : "",
+                age: 0
+            }
+        }))
     }
 
-    componentWillUnmount(){
-        //cleanup before component is destroyed
-    }
 
     render(){
         console.log(this.state)
         return(
-            <div>
-                <button onClick={this.handleClick}>Click</button>
-                {
-                    this.state.count === 5 && "count is 5"
-                }
-                {
-                    this.state.flag && <p>flag is true</p>
-                }
-            </div>
+        <div>
+            <form onSubmit={this.handleSubmit}>
+                <input type={"text"} name="name" placeholder="name" value={this.state.formData.name} onChange={this.handleNameChange}/>
+                <input type={"number"} name="age" placeholder="age" value={this.state.formData.age} onChange={this.handleAgeChange}/>
+                <button type={"submit"} aria-label="submit">submit</button>
+            </form>
+            {
+                this.state.finalFormData && Object.keys(this.state.finalFormData).length > 0 &&
+                <p>
+                                final data is {this.state.finalFormData.name} {this.state.finalFormData.age}
+                </p>
+            }
+        </div>
         )
     }
 };
