798279c17e028b957ccdedd53487a3f1e1b9d8ef
ri-ca4 <riley.de12@gmail.com>
2022-09-13 15:55:13 -0400

    practice with state

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -7,7 +7,6 @@ function App() {
   return (
     <div>
       <ClassBasedComponent/>
-      <FunctionBasedComponent/>
     </div>
   );
 }
--- first-react-app/src/components/ClassBasedComponent.jsx
+++ first-react-app/src/components/ClassBasedComponent.jsx
@@ -2,11 +2,44 @@ import React from "react";
 import { Component } from "react";
 
 class ClassBasedComponent extends Component {
+    
+    state = {
+        //write state properties here
+        count : 0 //initial value
+    }
+
+    // constructor(props){
+    //     super(props)
+    //     this.state = {
+    //         //write state properties here
+    //     }
+    //
+    //     this.handleClick = this.handleClick.bind(this)
+    // }
+
+    handleClick = () => {
+        //this.state.count = this.state.count + 1 <--- WRONG, directly mutating state
+        //USE setState methond!!!!!
+        const {count} = this.state;
+        this.setState({
+            count : count + 1
+        }, () => {
+            console.log(count)
+        })
+    }
+
     render(){
+        console.log(this.state)
         return(
-            <div>This is a class based component</div>
+            <div>
+                <button onClick={this.handleClick}>Click</button>
+                {
+                    this.state.count === 5 && "count is 5"
+                }
+            </div>
         )
     }
 };
 
 export default ClassBasedComponent;
+
